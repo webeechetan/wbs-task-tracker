b9012395794691bfbfedec300fddff3df5f76e7b
ajwebee1326 <ajay.kumar@webeesocial.com>
2023-10-04 16:03:19 +0530

    R &D on select2

--- resources/views/admin/activity/index.blade.php
+++ resources/views/admin/activity/index.blade.php
@@ -32,77 +32,72 @@
                 <div>
                     <label for="defaultFormControlInput" class="form-label">Activity Name</label>
                     <input type="text" class="form-control" id="defaultFormControlInput" placeholder="John Doe" aria-describedby="defaultFormControlHelp" fdprocessedid="a5svo">
-                    <div id="defaultFormControlHelp" class="form-text">We'll never share your details with anyone else.</div>
                 </div>
                 <div class="mt-3">
-                    <label for="" class="form-label">Team</label>
-                    <select id="xyz12" class="select2 form-select" multiple>
-                        <optgroup label="Alaskan/Hawaiian Time Zone">
+                    <label for="team" class="form-label">Team</label>
+                        <select id="team" class="select2 form-select" multiple>
+                            <option value="CA">California</option>
+                            <option value="NV">Nevada</option>
+                            <option value="OR">Oregon</option>
+                            <option value="WA">Washington</option>
+                        </select>
+                </div>
+
+                {{-- <div class="col-md-6 mb-4">
+                    <label for="select2Multiple" class="form-label">Multiple</label>
+                    <select id="select2Multiple" class="select2 form-select" multiple>
+                      <optgroup label="Alaskan/Hawaiian Time Zone">
                         <option value="AK">Alaska</option>
                         <option value="HI">Hawaii</option>
-                        </optgroup>
-                        <optgroup label="Pacific Time Zone">
-                        <option value="CA">California</option>
-                        <option value="NV">Nevada</option>
-                        <option value="OR">Oregon</option>
-                        <option value="WA">Washington</option>
-                        </optgroup>
-                        <optgroup label="Mountain Time Zone">
-                        <option value="AZ">Arizona</option>
-                        <option value="CO" selected>Colorado</option>
-                        <option value="ID">Idaho</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="UT">Utah</option>
-                        <option value="WY">Wyoming</option>
-                        </optgroup>
-                        <optgroup label="Central Time Zone">
-                        <option value="AL">Alabama</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TX">Texas</option>
-                        <option value="TN">Tennessee</option>
-                        <option value="WI">Wisconsin</option>
-                        </optgroup>
-                        <optgroup label="Eastern Time Zone">
-                        <option value="CT">Connecticut</option>
-                        <option value="DE">Delaware</option>
-                        <option value="FL" selected>Florida</option>
-                        <option value="GA">Georgia</option>
-                        <option value="IN">Indiana</option>
-                        <option value="ME">Maine</option>
-                        <option value="MD">Maryland</option>
-                        <option value="MA">Massachusetts</option>
-                        <option value="MI">Michigan</option>
-                        <option value="NH">New Hampshire</option>
-                        <option value="NJ">New Jersey</option>
-                        <option value="NY">New York</option>
-                        <option value="NC">North Carolina</option>
-                        <option value="OH">Ohio</option>
-                        <option value="PA">Pennsylvania</option>
-                        <option value="RI">Rhode Island</option>
-                        <option value="SC">South Carolina</option>
-                        <option value="VT">Vermont</option>
-                        <option value="VA">Virginia</option>
-                        <option value="WV">West Virginia</option>
-                        </optgroup>
+                      </optgroup>
                     </select>
-                </div>
+                  </div> --}}
             </div>
         </div>
     </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <div class="row">
     <div class="col-12">
@@ -381,7 +376,11 @@
 
 
     $(document).ready(function() {
-        $("#xyz12").select2();
+       
+        
+    $('#team').select2();
+
+
         let table = $('#activityTable').DataTable({
             responsive: true,
         });
@@ -464,6 +463,8 @@
             });
         });
 
+
+       
 
 </script>
 
